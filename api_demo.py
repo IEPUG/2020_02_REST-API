f0c856de44c6aa15e25c6d448579ce4aa908c169
John Sheehan <jennasyseng@gmail.com>
2020-02-17 23:05:09 -0800

    Misc updates

--- api_demo.py
+++ api_demo.py
@@ -1,10 +1,16 @@
 import requests
 
 
-def get_external_ip():
-    raw_data = requests.get("https://api.ipify.org/?format=json").json()  # {"ip":"12.34.56.78"}
-    return raw_data["ip"]
+def get_joke():
+    endpoint = "https://official-joke-api.appspot.com/jokes"
+    # joke_type = "/general"
+    joke_type = "/programming"
+    # joke_qty = "/ten"
+    joke_qty = "/random"
+
+    joke = requests.get(endpoint + joke_type + joke_qty).json()
+    return joke[0]["setup"], joke[0]["punchline"]
 
 
 if __name__ == "__main__":
-    print(get_external_ip())
+    print(get_joke())
