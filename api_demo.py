bd07e6154165fa7a107903cb3db3629c589ad1ea
John Sheehan <jennasyseng@gmail.com>
2022-06-21 20:37:13 -0700

    Updated joke base URL and print output

--- api_demo.py
+++ api_demo.py
@@ -1,16 +1,21 @@
 import requests
 
 
-def get_joke():
-    endpoint = "https://official-joke-api.appspot.com/jokes"
-    # joke_type = "general"
-    joke_type = "programming"
-    # joke_qty = "ten"
-    joke_qty = "random"
+def get_jokes():
+    # endpoint = "https://official-joke-api.appspot.com/jokes"
+    endpoint = "https://karljoke.herokuapp.com/jokes"
+    joke_type = "general"
+    # joke_type = "programming"
+    joke_qty = "ten"
+    # joke_qty = "random"
 
-    joke = requests.get('/'.join([endpoint, joke_type, joke_qty])).json()
-    return joke[0]["setup"], joke[0]["punchline"]
+    url = '/'.join([endpoint, joke_type, joke_qty])
+    print(url)
+    jokes = requests.get(url).json()
+    print(jokes)
+    return jokes
 
 
 if __name__ == "__main__":
-    print(get_joke())
+    for joke in get_jokes():
+        print(joke["setup"], joke["punchline"])
